fd18a6c32824cabc4d3ceb4994714d1afa7ea70f
kumarthapa <kumarthapa@kumarthapa>
2021-06-02 15:29:05 +0530

    deploying project on github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Signup from './component/sign up/Signup';
 import Login from './component/login/Login';
 import {AuthProvider} from './context/authContext';
